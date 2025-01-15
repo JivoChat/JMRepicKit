08762ec559d37fed542f046ca4b9f45d5e78b7e3
Stan Potemkin <stan@potemkin.su>
2025-01-15 09:27:55 +0300

    Make SPM Package

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "JMRepicKit",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "JMRepicKit",
+            targets: ["JMRepicKit"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/JivoChat/JMImageLoader.git", .branch("main")),
+    ],
+    targets: [
+        .target(
+            name: "JMRepicKit",
+            dependencies: [
+                .product(name: "JMImageLoader", package: "JMImageLoader"),
+            ],
+            path: "."
+        )
+    ]
+)
